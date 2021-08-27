04d24c8d5ae985fa8b115a8b23b0d59b01497bd8
Mahaboob Subhan Shaik <subhan150687@gmail.com>
2021-08-27 20:15:17 +0000

    Added data folders

--- flaskapp-docker/flaskapp/app.py
+++ flaskapp-docker/flaskapp/app.py
@@ -15,7 +15,7 @@ mail = Mail(app)
 
 @app.route("/")
 def home():
-    with open('items.json') as f:
+    with open('data/items.json') as f:
         item_list=json.load(f)
     #print(todo_list)
     return render_template("base.html", item_list=item_list)
@@ -26,17 +26,17 @@ def add():
     name = request.form.get("name")
     quantity = 0
     new_item = {"name":name, "quantity":quantity}
-    with open('items.json','r') as f:
+    with open('data/items.json','r') as f:
         item_list=json.load(f)
     item_list.append(new_item)
-    with open('items.json','w') as f:
+    with open('data/items.json','w') as f:
         json.dump(item_list,f)
     return redirect(url_for("home"))
 
 @app.route("/send", methods=["POST"])
 def send():
     msg = request.form.get("message")
-    with open('items.json') as f:
+    with open('data/items.json') as f:
         item_list=json.load(f)
     item_msg = msg + '\r\n'
     for item in item_list:
@@ -48,36 +48,28 @@ def send():
 
 @app.route("/update/<string:item_name>")
 def update(item_name):
-    with open('items.json','r') as f:
+    with open('data/items.json','r') as f:
         item_list=json.load(f)
     for item in item_list:
         if item["name"]==item_name:
             if item["quantity"]>=0 and item["quantity"]<=9:
                 item["quantity"]=item["quantity"]+1
             break
-    with open('items.json','w') as f:
+    with open('data/items.json','w') as f:
         json.dump(item_list,f)
     return redirect(url_for("home"))
 
 
 @app.route("/delete/<string:item_name>")
 def delete(item_name):
-    with open('items.json','r') as f:
+    with open('data/items.json','r') as f:
         item_list=json.load(f)
     for item in item_list:
         if item["name"]==item_name:
             if item["quantity"]!=0:
                 item["quantity"]=item["quantity"]-1
             break
-    with open('items.json','w') as f:
+    with open('data/items.json','w') as f:
         json.dump(item_list,f)
     return redirect(url_for("home"))
- #   todo = Todo.query.filter_by(id=todo_id).first()
-  #  db.session.delete(todo)
-   # db.session.commit()
-   # return redirect(url_for("home"))
 
-#if __name__ == "__main__":
-    #db.create_all()
-    #db.session.commit()
- #   app.run(debug=True)
